a2a75e3dbcfb17dd907d8e8a851f0847698110f4
SeungGyu Park <seunggyu.dev@gmail.com>
2023-08-21 16:08:02 +0900

    [Gold IV] Title: 민서의 응급 수술, Time: 20 ms, Memory: 2500 KB -BaekjoonHub

--- 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
+++ 백준/Gold/20955. 민서의 응급 수술/민서의 응급 수술.cc
@@ -1,43 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <string.h>
 #include <algorithm>
-#define ll long long
 #define endl "\n"
 using namespace std;
 
-vector<int> g[100'001];
-int vis[100'001];
+int p[100'100];
 
-void dfs(int root) {
-    vis[root] = 1;
-    for (int &nxt_child : g[root]) {
-        if (vis[nxt_child]) continue;
-        dfs(nxt_child);
-    }
+int find(int x) {
+    if (p[x] == x) return x;
+    return p[x] = find(p[x]);
 }
+
+void union_group(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a != b) p[b] = a;
+}
+
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
     // freopen("input.txt", "r", stdin);
 
     int n, m;
-    
+
     cin >> n >> m;
-    int tmp = m;
-    while (tmp--) {
+    for (int i = 1; i<=n;i++) p[i] = i;
+    
+    int res = 0;
+    while (m--) {
         int u, v;
         cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+
+        if (find(u) != find(v)) union_group(u, v);
+        else res++;
     }
-    int cnt = 0;
     for (int i = 1; i<=n;i++) {
-        if (vis[i]) continue;
-        dfs(i);
-        cnt++;
+        // 자기 자신 -> cycle 
+        if (i == find(i)) res++;
     }
-    cout << cnt-1 + (m + cnt - 1) - (n - 1);
+    cout << res -1;
     return 0;
 }
