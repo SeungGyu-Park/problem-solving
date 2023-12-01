544717e5c05292afc47b99133d562f4b491c2188
SeungGyu Park <seunggyu.dev@gmail.com>
2023-12-01 17:35:34 +0900

    [Gold III] Title: 우주 탐사선, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
@@ -1,61 +1,40 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <string.h>
 #include <algorithm>
+#include <vector>
 #define ll long long
 #define endl "\n"
 using namespace std;
 
 int n, k;
 int ana[11][11];
-vector<int> res;
+int ans = 1e9;
 
-void dfs(int cur, int vis[11], int sum) {
-    
-    int flag = 0;
-    for (int i = 0; i < n;i++) {
-        if (!vis[i]) flag = 1;
-    }
-
-    if (!flag) {
-        res.push_back(sum);
-        return;
-    }
-
-    int tmp[11];
-    for (int i = 0; i < n;i++) tmp[i] = vis[i];
-
-    for (int i = 0; i < n;i++) {
-        if (i == cur) continue;
-        if (tmp[i]) continue;
-        tmp[i] = 1;
-        dfs(i, tmp, sum + ana[cur][i]);
-        tmp[i] = 0;
-    }
-}
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
-    // freopen("input.txt", "r", stdin);
 
     cin >> n >> k;
 
-    for (int i = 0; i <n;i++)
+    vector<int> ord;
+    for (int i = 0; i <n;i++) {
+        if (i != k)
+            ord.push_back(i);
         for (int j = 0; j <n;j++)
             cin >> ana[i][j];
-
+    }
+        
     for (int k = 0; k < n;k++)
         for (int i = 0; i< n;i++) 
             for (int j = 0; j<n;j++) 
-                if (ana[i][k] + ana[k][j] < ana[i][j])
-                    ana[i][j] = ana[i][k] + ana[k][j];
+                ana[i][j] = min(ana[i][j], ana[i][k] + ana[k][j]);
 
-    int vis[11] = {0,};
-    vis[k] = 1;
-    dfs(k, vis, 0);
+    do {
+        int sum = ana[k][ord[0]];
+        for (int i = 0; i < ord.size() - 1; i++)
+            sum += ana[ord[i]][ord[i+1]];
+        ans = min(ans, sum);
+    } while (next_permutation(ord.begin(), ord.end()));
     
-    int ans = *min_element(res.begin(), res.end());
-    cout << ans << endl;
+    cout << ans;
     return 0;
 }
