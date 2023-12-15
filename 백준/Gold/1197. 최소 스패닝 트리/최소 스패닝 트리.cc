8db080a31736f71e5329c84c121bd79140bac45b
SeungGyu Park <seunggyu.dev@gmail.com>
2023-12-15 13:28:02 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 32 ms, Memory: 3236 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -7,20 +7,20 @@
 #define endl "\n"
 using namespace std;
 
-vector<tuple<int, int, int>> g;
-int p[10001];
+int r[10001];
+tuple<int, int, int> edge[100001];
 
 int find(int x) {
-    if (x == p[x]) return x;
-    return p[x] = find(p[x]);
+    if (x == r[x]) return x;
+    return r[x] = find(r[x]);
 }
 
 void uni(int a, int b) {
     a = find(a);
     b = find(b);
 
-    if (a < b) p[b] = a;
-    else p[a] = b;
+    if (a < b) r[b] = a;
+    else r[a] = b;
 }
 
 int main(void) {
@@ -31,25 +31,28 @@ int main(void) {
     int v, e;
 
     cin >> v >> e;
-    
-    for (int i = 0; i < e;i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        g.push_back({c, a, b});
-    }
 
-    sort(g.begin(), g.end());
-    for (int i = 1; i<=v;i++) p[i] = i;
+    for (int i =1;i <= v;i++) r[i] = i;
+
+    for (int i =0 ; i < e;i++) {
+        int a, b, c;
+
+        cin >> a >> b >> c;
+
+        edge[i] = {c, a, b};
+    }
+    sort(edge, edge + e);
 
     int ans = 0;
-    for (auto nxt : g) {
-        int c, a, b;
-        tie(c, a, b) = nxt;
-        if (find(a) != find(b)) {
-            ans += c;
-            uni(a, b);
-        }
+    for (int i =0 ; i < e;i++) {
+        int a, b, c;
+        tie(c, a, b) = edge[i];
+
+        if (find(a) == find(b)) continue;
+        uni(a, b);
+        ans += c;
     }
+
     cout << ans;
     return 0;
 }
