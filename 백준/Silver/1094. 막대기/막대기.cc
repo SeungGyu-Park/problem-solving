5e503c53969c4a12c48a3301690d9d87481905e6
SeungGyu Park <seunggyu.dev@gmail.com>
2023-12-30 15:50:44 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,4 +1,2 @@
-#include <iostream>
-using namespace std;
-int main(void) {
-int x,a=0,i=0;cin>>x;for(;i<7;++i)a+=(x&(1L<<i))!=0;cout<<a;return 0;}
+#include<iostream>
+int main(){int x,a=0,i=0;std::cin>>x;for(;i<7;++i)a+=(x&(1<<i))!=0;std::cout<<a;}
