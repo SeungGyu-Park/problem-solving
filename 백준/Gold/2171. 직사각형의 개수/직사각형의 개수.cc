69ad32a298e9411fc0a3bad2a0c85c136e8c7770
SeungGyu Park <seunggyu.dev@gmail.com>
2024-04-03 16:12:52 +0900

    [Gold V] Title: 직사각형의 개수, Time: 296 ms, Memory: 2688 KB -BaekjoonHub

--- 백준/Gold/2171. 직사각형의 개수/직사각형의 개수.cc
+++ 백준/Gold/2171. 직사각형의 개수/직사각형의 개수.cc
@@ -28,28 +28,15 @@ int main(void) {
 		m[x].push_back(y);
 	}
 
-	for (map<int, vector<int>>::iterator it = m.begin(); it != m.end(); ++it) {
+	for (auto it = m.begin(); it != m.end(); ++it)
 		sort(it->second.begin(), it->second.end());
-	}
 
 
-	for (map<int, vector<int>>::iterator i = m.begin(); i != m.end(); ++i) {
-		for (map<int, vector<int>>::iterator j = next(i); j != m.end(); ++j) {
+	for (auto i = m.begin(); i != m.end(); ++i) {
+		for (auto j = next(i); j != m.end(); ++j) {
 			
-			vector<int> x1 = i->second;
-			vector<int> x2 = j->second;
-
 			vector<int> cnt;
-			int p1 = 0, p2 = 0;
-			while (p1 < x1.size() && p2 < x2.size()) {
-
-				if (x1[p1] == x2[p2]) {
-					cnt.push_back(x1[p1]);
-					++p1; ++p2;
-				} else if (x1[p1] < x2[p2]) {
-					++p1;
-				} else ++p2;
-			}
+			set_intersection(i->second.begin(), i->second.end(), j->second.begin(), j->second.end(), back_inserter(cnt));
 			rec += (cnt.size() * (cnt.size() -1))/2;
 		}
 	}
