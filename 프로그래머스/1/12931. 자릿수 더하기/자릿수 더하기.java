4457519f44d881deb7a3674c22d5f078b8f0f5a3
SeungGyu Park <seunggyu.dev@gmail.com>
2024-04-30 16:59:02 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        
+        int ans = 0;
+        while (n != 0) {
+            ans += n%10;
+            n = n / 10;
+        }
+
+        return ans;
+    }
+}
